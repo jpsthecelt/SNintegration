edfb378b2ec34af094551ea1570a26fb5592861c
jpsthecelt <jpsthecelt@gmail.com>
2015-04-30 14:26:32 -0700

    Updated create to correct various errors

--- sNowIncidentCreate.py
+++ sNowIncidentCreate.py
@@ -7,10 +7,9 @@
 #
 
 import sys
+import json
 import requests
 from argparse import ArgumentParser
-#from time import strftime
-#import xml.etree.ElementTree as Et
 
 usage = """sNowIncidentCreate.py "problem description" [options]
 
@@ -22,10 +21,12 @@ Options:
   -h, --help                   Print this help message and exit
 
 Examples:
-  Reads & displays the first 10 ServiceNow Incident-Records
-  using the console login adminMO/adminmo.
+  Creates a new incident with the problem-description indicated on the command-line;
+        outputs sys_id of the created incident
 
-    sNowIncidentCreate -u adminMO -p adminmo
+  uses indicated service-now username & password
+
+    sNowIncidentCreate "problem description" -u adminMO -p adminmo
 
 """
 
@@ -34,8 +35,10 @@ if __name__ == '__main__':
     try:
         parser = ArgumentParser(add_help=False, usage=usage)
 
+        parser.add_argument('incident_desc')
         parser.add_argument('-u', '--user')
         parser.add_argument('-p', '--password')
+
 
         if '-h' in sys.argv or '--help' in sys.argv:
           print(usage)
@@ -62,8 +65,12 @@ if __name__ == '__main__':
             print('sNowIncidentGetinfo ERROR -- Status: ', r.status_code, 'Headers:', r.headers, 'Error Response:', r.json())
             exit()
         else:
-            print('Status:', r.status_code, 'Headers:', r.headers, 'Response:', r.json())
-            print('Cookies:', r.cookies)
+#            print('Status:', r.status_code, 'Headers:', r.headers, 'Response:', r.json())
+#            print('Cookies:', r.cookies)
+            dct = json.loads(r.text)
+            print "\nCreated Incident-Record {0} with SysID: {1}".format(dct['result']['number'], dct['result']['sys_id'])
+            print "\n (referencable URL is: %s)" % dct['result']['opened_by']['link']
+
 
 # Handle any exceptions, printing out error code
     except SystemExit:
